2adb0cc5c5ef16def335f5f22bffc27fef5ea8c1
Reyansh p <reyansh.pradeep@gmail.com>
2021-12-31 20:04:08 +0530

    all the methods u can use for lists

---
+++ Lists.py
@@ -0,0 +1,10 @@
+new_list=['Reyansh','Jhansi',"Pradeep",'Namratha']
+print(len(new_list)) #len() finds the length 
+new_list.append("Jhansi") #append() method to permanently add an item to the end of a list
+print(new_list)
+new_list.pop(1) #pop() removes a particular item in a list
+print(new_list)
+new_list.reverse()
+print(new_list)
+new_list.sort()
+print(new_list)
