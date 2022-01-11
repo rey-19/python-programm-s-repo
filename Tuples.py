9c2fd6e93b36e05a8eaeea099b6e447bcd2d5291
Reyansh p <reyansh.pradeep@gmail.com>
2022-01-11 23:28:51 +0530

    learning changes

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+#tupples is an imutable type of data 
+t=('reyansh','jhansi','pradeep','namratha','reyansh')
+print(t[0])
+print(t[::1])#slicing
+print(len(t))#len
+print(t.index('pradeep'))#finds the index position of the given strings 
+print(t.count('reyansh'))#counts the num of times a string occurs 
